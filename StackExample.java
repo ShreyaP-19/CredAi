631b83ac8e0b491bfed37500456185d885fa12cf
ShreyaP-19 <shreyapera@gmail.com>
2026-01-21 12:08:22 +0530

    Create StackExample.java

---
+++ StackExample.java
@@ -0,0 +1,17 @@
+import java.util.Stack;
+
+public class StackExample {
+    public static void main(String[] args){
+        Stack<Integer> s = new Stack<Integer>();
+
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+
+        System.out.println("Stack:"+s);
+        System.out.println("Popped element:"+s.pop());
+        System.out.println("Stack:"+s);
+        System.out.println("Top element:"+s.peek());
+    }
+}
